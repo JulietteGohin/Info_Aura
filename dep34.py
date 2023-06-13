d7d2d88521daaf74817dbf3d4778170347b0ed9e
Juliette Gohin <juliette.gohin@etu.minesparis.psl.eu>
2023-06-13 17:13:19 +0200

    Opening the geopackage file once, when creating an instance of Stats
    It allows to reduce the running time

--- dep34.py
+++ dep34.py
@@ -77,7 +77,7 @@ class Data:
         """
         minimal_data = 'minimal_data.gpkg'
         gdf = gpd.read_file(light_data)
-        gdf = gdf[['batiment_groupe_id', 'code_departement_insee', 's_geom_groupe', 'code_iris', 'code_commune_insee', 'libelle_commune_insee', 'code_epci_insee', 'contient_fictive_geom_groupe', 'cle_interop_adr_principale_ban', 'libelle_adr_principale_ban', 'nb_adresse_valid_ban', 'fiabilite_cr_adr_niv_1', 'fiabilite_cr_adr_niv_2', 'argiles_alea', 'bdtopo_bat_l_nature', 'bdtopo_bat_l_usage_1', 'bdtopo_bat_l_usage_2', 'bdtopo_bat_l_etat', 'bdtopo_bat_hauteur_mean', 'bdtopo_bat_altitude_sol_mean', 'bdtopo_equ_l_nature', 'bdtopo_equ_l_nature_detaillee', 'bdtopo_equ_l_toponyme', 'bdtopo_zoa_l_nature', 'bdtopo_zoa_l_nature_detaillee', 'bdtopo_zoa_l_toponyme', 'bpe_l_type_equipement', 'dle_elec_2020_nb_pdl_res', 'dle_elec_2020_nb_pdl_pro', 'dle_elec_2020_nb_pdl_tot', 'dle_elec_2020_conso_res', 'dle_elec_2020_conso_pro', 'dle_elec_2020_conso_tot', 'dle_elec_2020_conso_res_par_pdl', 'dle_elec_2020_conso_pro_par_pdl', 'dle_elec_2020_conso_tot_par_pdl', 'dle_gaz_2020_nb_pdl_res', 'dle_gaz_2020_nb_pdl_pro', 'dle_gaz_2020_nb_pdl_tot', 'dle_gaz_2020_conso_res', 'dle_gaz_2020_conso_pro', 'dle_gaz_2020_conso_tot', 'dle_gaz_2020_conso_res_par_pdl', 'dle_gaz_2020_conso_pro_par_pdl', 'dle_gaz_2020_conso_tot_par_pdl', 'dle_reseaux_2020_nb_pdl_res', 'dle_reseaux_2020_nb_pdl_pro', 'dle_reseaux_2020_nb_pdl_tot', 'dle_reseaux_2020_conso_res', 'dle_reseaux_2020_conso_pro', 'dle_reseaux_2020_conso_tot', 'dle_reseaux_2020_conso_res_par_pdl', 'dle_reseaux_2020_conso_pro_par_pdl', 'dle_reseaux_2020_conso_tot_par_pdl', 'dle_reseaux_2020_identifiant_reseau', 'dle_reseaux_2020_type_reseau', 'dpe_nb_classe_ener_a', 'dpe_nb_classe_ener_b', 'dpe_nb_classe_ener_c', 'dpe_nb_classe_ener_d', 'dpe_nb_classe_ener_e', 'dpe_nb_classe_ener_f', 'dpe_nb_classe_ener_g', 'dpe_nb_classe_ener_nc', 'dpe_nb_classe_ges_a', 'dpe_nb_classe_ges_b', 'dpe_nb_classe_ges_c', 'dpe_nb_classe_ges_d', 'dpe_nb_classe_ges_e', 'dpe_nb_classe_ges_f', 'dpe_nb_classe_ges_g', 'dpe_nb_classe_ges_nc', 'dpe_conso_ener_mean', 'dpe_estim_ges_mean', 'dpe_conso_ener_std', 'dpe_estim_ges_std', 'dpe_conso_ener_min', 'dpe_estim_ges_min', 'dpe_conso_ener_max', 'dpe_estim_ges_max', 'dpe_class_conso_ener_mean', 'dpe_class_estim_ges_mean', 'dpe_l_ch_gen_princ', 'dpe_l_ecs_gen_princ', 'dpe_logtype_dpe_id', 'dpe_logtype_numero_dpe', 'dpe_logtype_avancee_masque_max', 'dpe_logtype_baie_fs', 'dpe_logtype_baie_mat', 'dpe_logtype_baie_orientation', 'dpe_logtype_baie_remplissage', 'dpe_logtype_baie_type_vitrage', 'dpe_logtype_baie_u', 'dpe_logtype_ch_gen_lib', 'dpe_logtype_ch_gen_lib_appoint', 'dpe_logtype_ch_gen_lib_princ', 'dpe_logtype_ch_solaire', 'dpe_logtype_ch_type_ener_corr', 'dpe_logtype_ch_type_inst', 'dpe_logtype_classe_conso_ener', 'dpe_logtype_classe_estim_ges', 'dpe_logtype_coherence_donnees_dpe', 'dpe_logtype_conso_ener', 'dpe_logtype_date_reception_dpe', 'dpe_logtype_ecs_gen_lib', 'dpe_logtype_ecs_gen_lib_appoint', 'dpe_logtype_ecs_gen_lib_princ', 'dpe_logtype_ecs_solaire', 'dpe_logtype_ecs_type_ener', 'dpe_logtype_ecs_type_inst', 'dpe_logtype_enr', 'dpe_logtype_estim_ges', 'dpe_logtype_inertie', 'dpe_logtype_methode_3cl', 'dpe_logtype_traversant', 'dpe_logtype_mur_ep_mat_ext', 'dpe_logtype_mur_mat_ext', 'dpe_logtype_mur_pos_isol_ext', 'dpe_logtype_mur_u_ext', 'dpe_logtype_nom_methode_dpe', 'dpe_logtype_pb_mat', 'dpe_logtype_pb_pos_isol', 'dpe_logtype_pb_type_adjacence', 'dpe_logtype_pb_u', 'dpe_logtype_prc_s_vitree_ext', 'dpe_logtype_periode_construction', 'dpe_logtype_ph_mat', 'dpe_logtype_ph_pos_isol', 'dpe_logtype_ph_type_adjacence', 'dpe_logtype_ph_u', 'dpe_logtype_presence_balcon', 'dpe_logtype_presence_climatisation', 'dpe_logtype_s_hab', 'dpe_logtype_type_batiment', 'dpe_logtype_type_ventilation', 'dpe_logtype_ratio_ges_conso', 'ffo_bat_annee_construction', 'ffo_bat_usage_niveau_1_txt', 'ffo_bat_mat_mur_txt', 'ffo_bat_mat_toit_txt', 'ffo_bat_nb_log', 'fiabilite_emprise_sol', 'fiabilite_hauteur', 'fiabilite_adresse', 'croisement_geospx_reussi', 'hthd_nb_pdl', 'hthd_l_type_pdl', 'hthd_l_nom_pdl', 'merimee_distance_batiment_historique_plus_proche', 'merimee_nom_batiment_historique_plus_proche', 'qpv_nom_quartier', 'radon_alea', 'rnc_ope_numero_immat_principal', 'rnc_ope_periode_construction_max', 'rnc_ope_l_annee_construction', 'rnc_ope_nb_lot_garpark', 'rnc_ope_nb_lot_tot', 'rnc_ope_nb_log', 'rnc_ope_nb_lot_tertiaire', 'rnc_ope_l_nom_copro', 'geometry']]
+        gdf = gdf[['batiment_groupe_id', 'code_departement_insee', 's_geom_groupe', 'code_iris', 'code_commune_insee', 'libelle_commune_insee', 'code_epci_insee', 'contient_fictive_geom_groupe', 'cle_interop_adr_principale_ban', 'libelle_adr_principale_ban', 'nb_adresse_valid_ban', 'fiabilite_cr_adr_niv_1', 'fiabilite_cr_adr_niv_2', 'argiles_alea', 'bdtopo_bat_l_nature', 'bdtopo_bat_l_usage_1', 'bdtopo_bat_l_usage_2', 'bdtopo_bat_l_etat', 'bdtopo_bat_hauteur_mean', 'bdtopo_bat_altitude_sol_mean', 'bdtopo_equ_l_nature', 'bdtopo_equ_l_nature_detaillee', 'bdtopo_equ_l_toponyme', 'bdtopo_zoa_l_nature', 'bdtopo_zoa_l_nature_detaillee', 'bdtopo_zoa_l_toponyme', 'bpe_l_type_equipement', 'dle_2020_elec_nb_pdl_res', 'dle_elec_2020_nb_pdl_pro', 'dle_elec_2020_nb_pdl_tot', 'dle_elec_2020_conso_res', 'dle_elec_2020_conso_pro', 'dle_elec_2020_conso_tot', 'dle_elec_2020_conso_res_par_pdl', 'dle_elec_2020_conso_pro_par_pdl', 'dle_elec_2020_conso_tot_par_pdl', 'dle_gaz_2020_nb_pdl_res', 'dle_gaz_2020_nb_pdl_pro', 'dle_gaz_2020_nb_pdl_tot', 'dle_gaz_2020_conso_res', 'dle_gaz_2020_conso_pro', 'dle_gaz_2020_conso_tot', 'dle_gaz_2020_conso_res_par_pdl', 'dle_gaz_2020_conso_pro_par_pdl', 'dle_gaz_2020_conso_tot_par_pdl', 'dle_reseaux_2020_nb_pdl_res', 'dle_reseaux_2020_nb_pdl_pro', 'dle_reseaux_2020_nb_pdl_tot', 'dle_reseaux_2020_conso_res', 'dle_reseaux_2020_conso_pro', 'dle_reseaux_2020_conso_tot', 'dle_reseaux_2020_conso_res_par_pdl', 'dle_reseaux_2020_conso_pro_par_pdl', 'dle_reseaux_2020_conso_tot_par_pdl', 'dle_reseaux_2020_identifiant_reseau', 'dle_reseaux_2020_type_reseau', 'dpe_nb_classe_ener_a', 'dpe_nb_classe_ener_b', 'dpe_nb_classe_ener_c', 'dpe_nb_classe_ener_d', 'dpe_nb_classe_ener_e', 'dpe_nb_classe_ener_f', 'dpe_nb_classe_ener_g', 'dpe_nb_classe_ener_nc', 'dpe_nb_classe_ges_a', 'dpe_nb_classe_ges_b', 'dpe_nb_classe_ges_c', 'dpe_nb_classe_ges_d', 'dpe_nb_classe_ges_e', 'dpe_nb_classe_ges_f', 'dpe_nb_classe_ges_g', 'dpe_nb_classe_ges_nc', 'dpe_conso_ener_mean', 'dpe_estim_ges_mean', 'dpe_conso_ener_std', 'dpe_estim_ges_std', 'dpe_conso_ener_min', 'dpe_estim_ges_min', 'dpe_conso_ener_max', 'dpe_estim_ges_max', 'dpe_class_conso_ener_mean', 'dpe_class_estim_ges_mean', 'dpe_l_ch_gen_princ', 'dpe_l_ecs_gen_princ', 'dpe_logtype_dpe_id', 'dpe_logtype_numero_dpe', 'dpe_logtype_avancee_masque_max', 'dpe_logtype_baie_fs', 'dpe_logtype_baie_mat', 'dpe_logtype_baie_orientation', 'dpe_logtype_baie_remplissage', 'dpe_logtype_baie_type_vitrage', 'dpe_logtype_baie_u', 'dpe_logtype_ch_gen_lib', 'dpe_logtype_ch_gen_lib_appoint', 'dpe_logtype_ch_gen_lib_princ', 'dpe_logtype_ch_solaire', 'dpe_logtype_ch_type_ener_corr', 'dpe_logtype_ch_type_inst', 'dpe_logtype_classe_conso_ener', 'dpe_logtype_classe_estim_ges', 'dpe_logtype_coherence_donnees_dpe', 'dpe_logtype_conso_ener', 'dpe_logtype_date_reception_dpe', 'dpe_logtype_ecs_gen_lib', 'dpe_logtype_ecs_gen_lib_appoint', 'dpe_logtype_ecs_gen_lib_princ', 'dpe_logtype_ecs_solaire', 'dpe_logtype_ecs_type_ener', 'dpe_logtype_ecs_type_inst', 'dpe_logtype_enr', 'dpe_logtype_estim_ges', 'dpe_logtype_inertie', 'dpe_logtype_methode_3cl', 'dpe_logtype_traversant', 'dpe_logtype_mur_ep_mat_ext', 'dpe_logtype_mur_mat_ext', 'dpe_logtype_mur_pos_isol_ext', 'dpe_logtype_mur_u_ext', 'dpe_logtype_nom_methode_dpe', 'dpe_logtype_pb_mat', 'dpe_logtype_pb_pos_isol', 'dpe_logtype_pb_type_adjacence', 'dpe_logtype_pb_u', 'dpe_logtype_prc_s_vitree_ext', 'dpe_logtype_periode_construction', 'dpe_logtype_ph_mat', 'dpe_logtype_ph_pos_isol', 'dpe_logtype_ph_type_adjacence', 'dpe_logtype_ph_u', 'dpe_logtype_presence_balcon', 'dpe_logtype_presence_climatisation', 'dpe_logtype_s_hab', 'dpe_logtype_type_batiment', 'dpe_logtype_type_ventilation', 'dpe_logtype_ratio_ges_conso', 'ffo_bat_annee_construction', 'ffo_bat_usage_niveau_1_txt', 'ffo_bat_mat_mur_txt', 'ffo_bat_mat_toit_txt', 'ffo_bat_nb_log', 'fiabilite_emprise_sol', 'fiabilite_hauteur', 'fiabilite_adresse', 'croisement_geospx_reussi', 'hthd_nb_pdl', 'hthd_l_type_pdl', 'hthd_l_nom_pdl', 'merimee_distance_batiment_historique_plus_proche', 'merimee_nom_batiment_historique_plus_proche', 'qpv_nom_quartier', 'radon_alea', 'rnc_ope_numero_immat_principal', 'rnc_ope_periode_construction_max', 'rnc_ope_l_annee_construction', 'rnc_ope_nb_lot_garpark', 'rnc_ope_nb_lot_tot', 'rnc_ope_nb_log', 'rnc_ope_nb_lot_tertiaire', 'rnc_ope_l_nom_copro', 'geometry']]
         gdf.to_file(minimal_data, driver='GPKG')
         return minimal_data
 
@@ -89,6 +89,7 @@ class Stats:
     def __init__(self, data):
         #data is minimal_data
         self.data = data
+        self.gdf = gpd.read_file(self.data)
 
 
     def hist_height_city(self, postal_code):
@@ -100,10 +101,12 @@ class Stats:
         postal_code : str 
         
         """
-        gdf = gpd.read_file(self.data)
-        gdf = gdf.groupby(by='code_commune_insee')
+        gdf = self.gdf.groupby(by='code_commune_insee')
         sub_gdf = gdf.get_group(postal_code) 
-        sub_gdf.hist('bdtopo_bat_hauteur_mean', bins=10, legend = True)
+        sub_gdf.hist('bdtopo_bat_hauteur_mean', bins=20)
+        plt.title(f"Histogramme de la hauteur des batiments de la ville {postal_code}")
+        plt.xlabel("Hauteur des batiments (m)")
+        plt.ylabel("Nombre de batiments")
         plt.show()
     
 
@@ -118,10 +121,10 @@ class Stats:
         Output:
         mean height of the buildings in the chosen city
         """
-        gdf = gpd.read_file(self.data)
-        gdf = gdf.groupby(by='code_commune_insee')
+        gdf = self.gdf.groupby(by='code_commune_insee')
         sub_gdf = gdf.get_group(postal_code)
         mean_height = sub_gdf['bdtopo_bat_hauteur_mean'].mean()
+        print(f"La hauteur moyenne des batiments de la ville {postal_code} est de {round(mean_height, 2)} m"
         return mean_height
     
 
@@ -132,15 +135,14 @@ class Stats:
         ---
         Output: 
         plot circular graphic of the DPE in the department
-        """
-        gdf = gpd.read_file(self.data)
-        A = gdf['dpe_nb_classe_ener_a'].sum()
-        B = gdf['dpe_nb_classe_ener_b'].sum()
-        C = gdf['dpe_nb_classe_ener_c'].sum()
-        D = gdf['dpe_nb_classe_ener_d'].sum()
-        E = gdf['dpe_nb_classe_ener_e'].sum()
-        F = gdf['dpe_nb_classe_ener_f'].sum()
-        G = gdf['dpe_nb_classe_ener_g'].sum()
+        """ 
+        A = self.gdf['dpe_nb_classe_ener_a'].sum()
+        B = self.gdf['dpe_nb_classe_ener_b'].sum()
+        C = self.gdf['dpe_nb_classe_ener_c'].sum()
+        D = self.gdf['dpe_nb_classe_ener_d'].sum()
+        E = self.gdf['dpe_nb_classe_ener_e'].sum()
+        F = self.gdf['dpe_nb_classe_ener_f'].sum()
+        G = self.gdf['dpe_nb_classe_ener_g'].sum()
         dpe_gdf = pd.DataFrame({'DPE': [A, B, C, D, E, F, G]}, index=['A', 'B', 'C', 'D', 'E', 'F', 'G'])
         dpe_gdf.plot.pie(y='DPE', figsize=(5, 5), title="Répartition des DPE dans le département")
 
@@ -156,7 +158,7 @@ class Stats:
         Output: 
         plot circular graphic of the DPE in the department
         """
-        gdf = gpd.read_file(self.data).groupby(by='code_commune_insee')
+        gdf = self.gdf.groupby(by='code_commune_insee')
         gdf = gdf.get_group(postal_code)
         A = gdf['dpe_nb_classe_ener_a'].sum()
         B = gdf['dpe_nb_classe_ener_b'].sum()
@@ -172,7 +174,7 @@ class Stats:
 
     def climatisation_rate(self, postal_code):
         """
-        Compute the rate of buildings with air conditioning in the department
+        Compute the rate of buildings with air conditioning in the department and in the city of interest
 
         ---
         Parameters : 
@@ -181,11 +183,10 @@ class Stats:
         Output:
         % of buildings with air conditioning in the department
         """
-        gdf = gpd.read_file(self.data)
-        gdf_clim = gdf[gdf['dpe_logtype_presence_climatisation'] == True]
-        rate = len(gdf_clim)/len(gdf)*100
+        gdf_clim = self.gdf[self.gdf['dpe_logtype_presence_climatisation'] == True]
+        rate = len(gdf_clim)/len(self.gdf)*100
 
-        gdf_city = gdf[gdf['code_commune_insee'] == postal_code]
+        gdf_city = self.gdf[self.gdf['code_commune_insee'] == postal_code]
         gdf_city_clim = gdf_city[gdf_city['dpe_logtype_presence_climatisation'] == True]
         rate_city = len(gdf_city_clim)/len(gdf_city)*100
         print(f"Le taux de batiments climatises est de {round(rate, 2)}% dans le departement, et de {round(rate_city, 2)} dans la ville d'interet")
@@ -197,10 +198,10 @@ if __name__ == "__main__":
     minimal_data = 'light_building.gpkg'
     stats = Stats(minimal_data)
     stats.dpe_departement()
-    stats.dpe_city('69123') #Lyon 
-    stats.hist_height_city('69123')
-    stats.mean_height_city('69123')
-    stats.climatisation_rate('69123')
+    #stats.dpe_city('69123') #Lyon 
+    #stats.hist_height_city('69123')
+    #stats.mean_height_city('69123')
+    #stats.climatisation_rate('69123')
 
 
 
